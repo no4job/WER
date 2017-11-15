8554519a21d21e4e427b430d7a43f3f83b611769
mdu <no_4job@gmail.com>
2017-11-15 12:37:22 +0300

    asr first version

--- SRC/ASR.py
+++ SRC/ASR.py
@@ -1,12 +1,10 @@
 import subprocess
 import sys
 import os
-# asrclient_cli = __import__("asrclient-cli")
-import asrclient_cli
-# *******************
-# imported_module = __import__("advanced_callback_example", globals(), locals(), [], 0)
-# *********************
-import codecs
+import pickle
+
+IPC_FILE = '..\\IPC\\asr_responce_lst.pickle'
+
 '''
 DEFAULT_KEY_VALUE = 'paste-your-own-key'
 DEFAULT_SERVER_VALUE = 'asr.yandex.net'
@@ -30,13 +28,12 @@ DEFAULT_CMN_LATENCY = 50
 '''
 '''
 1. pip install click
-2. pip install protobuf-3.4.0-py3-none-any.whl - ???
 3. from https://github.com/google/protobuf/releases 
     download  protoc-3.4.0-win32.zip
     unpack to xxx\yyy\zzz path
 4. download speechkitcloud from github as zip file and unpack it in aaa\bbb\ccc
 5.
-install speechkitcloud according README.txt:
+install speechkitcloud according README.txt(https://github.com/yandex/speechkitcloud/tree/master/python):
 cd aaa\bbb\ccc\speechkitcloud/python
 xxx\yyy\zzz\bin\protoc -I=asrclient --python_out=asrclient asrclient/*.proto
 python ./setup.py sdist
@@ -45,40 +42,129 @@ sudo pip install <generated-file-name>
 
 6. copy asrclient-cli.py to project src folder
 
-7. Change file in installed site package installed file 
-C:/virtual_envs/WER/Lib/site-packages/asrclient/basic_pb2.py
+7. Change site package  files: 
+C:/virtual_envs/WER/Lib/site-packages/asrclient/voiceproxy_pb2.py
 change string 16 :  import basic_pb2 as basic__pb2 -> from . import basic_pb2 as basic__pb2 
 
+C:\virtual_envs\WER\Lib\site-packages\asrclient\client.py
+change string 264 : 
+from:
+imported_module = __import__(callback_module, globals(), locals(), [], -1)
+to:
+imported_module = __import__(callback_module, globals(), locals(), [], 0)
 '''
+class AsrClientOptions:
+    def __init__(self, **kwargs):
+        self.option_defaults = {
+            'key': ['--key','924df669-0866-4524-b2ed-1e6c2eda6867'],
+            'model':['--model','freeform'],
+            'callback_module':['--callback-module','advanced_callback'],
+            'realtime':['--realtime',False] ,
+            'nopunctuation': ['--nopunctuation',True],
+            'silent' : ['--silent',True]
+        }
+        # for (option, default) in option_defaults.items():
+        #     setattr(self, option, kwargs.get(option, default))
+        self.test_option(kwargs)
+        self.options={}
+        for (option, default) in self.option_defaults.items():
+            self.options[option] = kwargs.get(option, default)
+
+
+    def option_str(self):
+        options_str =""
+        for option, value in self.options.items():
+            if type(value[1]) != bool:
+                options_str =  options_str+" {} {}".format(value[0], value[1])
+            else:
+                if value[1]:
+                    options_str =  options_str+" {}".format(value[0])
+                # options_str = " ".join( "{} {}".format(value[0], value[1]) for option, value in self.options.items())
+        return options_str
+
+    def test_option(self,kwargs):
+        for option, value in kwargs.items():
+            if option not in self.option_defaults or type(value) != type( self.option_defaults[option][1]):
+                raise ValueError('Bad parameter: {}:{}'.format(option,value))
+
+    def set_option(self,**kwargs):
+        self.test_option(kwargs)
+        for (option, value) in self.options.items():
+            self.options[option][1] = kwargs.get(option, value[1])
+
+    def get_option(self,option):
+        return self.options[option][1]
+
+def recognize(file,options=None):
+    if not options:
+        options = AsrClientOptions().option_str()
+    process = subprocess.Popen(sys.executable + " asrclient-cli.py"+ " " + options+ " "+
+                               file,
+                               shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    stdout = process.communicate()[0]
+    stderr = process.communicate()[1]
+    print (stderr.decode(encoding="cp866"))
+    print (stdout.decode(encoding="utf-8"))
+    if process.returncode != 0:
+        print ("Execution failed, return code: {}".format(process.returncode))
+        exit (process.returncode)
+    asr_response_lst = []
+    with open(IPC_FILE, 'rb') as f:
+        asr_response_lst = pickle.load(f)
+    all_text = ""
+    for response in asr_response_lst:
+        if len(response):
+            utterance = " ".join(word['value']  for word in response[0]['word'])
+            all_text =  all_text + utterance + "\n"
+    # print (all_text)
+    return all_text
+
+
 if __name__ == "__main__":
+    AUDIO_IN_DIR_PATH = '../AUDIO_IN/'
+    AUDIO_FILE_NAME = 'all_text_16_my_voice.wav'
+    file = os.path.abspath(AUDIO_IN_DIR_PATH+AUDIO_FILE_NAME)
+    print(recognize(file))
+    exit (0)
+
+
+    '''
+    options = AsrClientOptions().option_str()
+    print(options)
+    # exit(0)
     AUDIO_IN_DIR_PATH = '../AUDIO_IN/'
     AUDIO_FILE_NAME = 'all_text_16_my_voice.wav'
     RECOGNIZED_DIR_PATH = '../RECOGNIZED_OUT/'
     RECOGNIZED_FILE_NAME = 'recognized.txt'
     API_KEY = '924df669-0866-4524-b2ed-1e6c2eda6867'
     MODEL = 'freeform'
-    cbck_module  = "advanced_callback_example"
+    cbck_module  = "advanced_callback"
     # realtime = "--realtime"
+    no_punctuation = "--nopunctuation"
+    silent = "--silent"
     realtime = ""
-    arg = '--key {} --model {} --callback-module {} {} {}'.format(API_KEY, MODEL, cbck_module,realtime,os.path.abspath(AUDIO_IN_DIR_PATH+AUDIO_FILE_NAME))
-    # print(sys.executable)
-    # sys.stdout = codecs.getwriter('cp866')(sys.stdout,'replace')
-    # try:
-    # retcode =  subprocess.call(sys.executable + "asrclient-cli.py" + " arg", shell=True)
-    #retcode =  subprocess.call(sys.executable + "asrclient-cli.py" + " arg", shell=False)
-    # retcode =  subprocess.check_output(sys.executable + "asrclient-cli.py" + " arg", shell=True)
-    process = subprocess.Popen(sys.executable + " asrclient-cli.py" + " " + arg, shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    arg = '--key {} --model {} --callback-module {} {} {} {} {}'.format(API_KEY, MODEL, cbck_module,realtime,silent,
+                                                no_punctuation,os.path.abspath(AUDIO_IN_DIR_PATH+AUDIO_FILE_NAME))
+    # process = subprocess.Popen(sys.executable + " asrclient-cli.py" + " " + arg, shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+    process = subprocess.Popen(sys.executable + " asrclient-cli.py"+ " " + options+ " "+
+                                os.path.abspath(AUDIO_IN_DIR_PATH+AUDIO_FILE_NAME),
+                               shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+
     stdout = process.communicate()[0]
     stderr = process.communicate()[1]
-    # ******print (stdout.decode(encoding="cp866"))
     print (stderr.decode(encoding="cp866"))
     print (stdout.decode(encoding="utf-8"))
-    # print (stderr.decode(encoding="utf-8"))
     if process.returncode != 0:
         print ("Execution failed, return code: {}".format(process.returncode))
-    # except OSError as e:
-    #     print("Execution failed:", e)
+        exit (process.returncode)
+    asr_response_lst = []
+    with open(IPC_FILE, 'rb') as f:
+        asr_response_lst = pickle.load(f)
+    all_text = ""
+    for response in asr_response_lst:
+      if len(response):
+          utterance = " ".join(word['value']  for word in response[0]['word'])
+          all_text =  all_text + utterance + "\n"
 
-    # print (sys.argv)
-    # asrclient_cli.main_(arg)
-    # asrclient_cli.main()
+    print (all_text)
+    '''
---
+++ SRC/advanced_callback.py
@@ -0,0 +1,78 @@
+from asrclient.voiceproxy_pb2 import AddDataResponse as AsrResponse
+import pickle
+# import dill
+# import sys
+import os
+IPC_FILE = '..\\IPC\\asr_responce_lst.pickle'
+"""
+use it like
+./asrclient-cli.py -k <your-key> --callback-module advanced_callback_example --silent <path-to-your-sound.wav>
+"""
+
+try:
+    os.remove(IPC_FILE)
+except OSError:
+    pass
+asr_response_lst = []
+call_count = 0
+session_id = "not-set"
+def advanced_callback(asr_response, correction = 0):
+    global asr_response_lst
+    if asr_response.endOfUtt:
+        r_count = 0
+        response = []
+        for r in asr_response.recognition:
+            hyp = {}
+            # word = {}
+            hyp['confidence']= r.confidence
+            hyp['normalized']= r.normalized
+            hyp['start_time']= r.align_info.start_time+correction
+            hyp['end_time']= r.align_info.end_time+correction
+            # print("recognition[{}] = {}; confidence = {}".format(r_count, r.normalized, r.confidence))
+            # print("utterance timings: from {} to {}".format(r.align_info.start_time+correction,r.align_info.end_time+correction))
+            hyp['word'] = []
+            w_count = 0
+            for w in r.words:
+                word ={}
+                word['confidence']= w.confidence
+                word['value']= w.value
+                word['start_time']= w.align_info.start_time+correction
+                word['end_time']= w.align_info.end_time+correction
+                # print("word[{}] = {}; confidence = {}".format(w_count, w.value.encode("utf-8"), w.confidence))
+                # print("word[{}] = {}; confidence = {}".format(w_count, w.value, w.confidence))
+                # print("word timings: from {} to {}".format(w.align_info.start_time+correction,w.align_info.end_time+correction))
+                hyp['word'].append(word)
+                w_count += 1
+            r_count += 1
+            response.append(hyp)
+        asr_response_lst.append(response)
+        # with open('..\\IPC\\asr_responce_lst_{}.pickle'.format(call_count), 'wb') as f:
+        with open(IPC_FILE, 'wb') as f:
+            pickle.dump(asr_response_lst, f)
+'''
+    print("Got response:")
+    print("end-of-utterance = {}".format(asr_response.endOfUtt))
+    r_count = 0
+    for r in asr_response.recognition:
+        # print("recognition[{}] = {}; confidence = {}".format(r_count, r.normalized.encode("utf-8"), r.confidence))
+        print("recognition[{}] = {}; confidence = {}".format(r_count, r.normalized, r.confidence))
+        # print("recognition[{}] = {}; confidence = {}".format(r_count, r.normalized.encode("utf-8"), r.confidence))
+        print("utterance timings: from {} to {}".format(r.align_info.start_time+correction,r.align_info.end_time+correction))
+        w_count = 0
+        for w in r.words:
+            # print("word[{}] = {}; confidence = {}".format(w_count, w.value.encode("utf-8"), w.confidence))
+            print("word[{}] = {}; confidence = {}".format(w_count, w.value, w.confidence))
+            print("word timings: from {} to {}".format(w.align_info.start_time+correction,w.align_info.end_time+correction))
+            w_count += 1
+        r_count += 1
+'''
+def advanced_utterance_callback(asr_response, data_chunks):
+    pass
+
+# def advanced_utterance_callback(asr_response, data_chunks):
+#     data_length = 0
+#     for chunk in data_chunks:
+#         data_length += len(chunk) if chunk else 0
+#     print("Got complete utterance, for {0} data_chunks, session_id = {1}".format(len(data_chunks), session_id))
+#     print("Metainfo", asr_response.metainfo.minBeam, asr_response.metainfo.maxBeam)
+#     print("Data length = {0}".format(data_length))
